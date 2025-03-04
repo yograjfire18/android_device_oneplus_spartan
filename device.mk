96604ef9acbf78a3023df7524030aa31d99708fe
yograjfire18 <yograjfire18@gmail.com>
2025-03-04 19:02:59 +0530

    spartan: Build libutils into vendor

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_SOONG_NAMESPACES += \
 # Touch
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
 
+# VNKDK
+PRODUCT_PACKAGES += \
+    libutils.vendor
+
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/oneplus/sm8250-common/common.mk)
 
